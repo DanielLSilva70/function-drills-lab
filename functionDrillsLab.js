a035629fee26ec806f31ae47f85b13c3c2e7d217
Daniel Silva <silvaldaniel70@gmail.com>
2022-01-11 21:07:28 -0600

    19 done

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -372,7 +372,17 @@ console.log(emailCheck(email))
 
 //CODE HERE
 
-
+function frogCost(gold) {
+  if(gold % 3 === 0) {
+    return gold / 3
+  }else if ((gold - 1) % 3 === 0) {
+    return (gold - 1) / 3
+  } else {
+    return (gold - 2) / 3
+  }
+}
+let totalFrogs = frogCost(9)
+console.log(totalFrogs)
 ////////////////// PROBLEM 20 ////////////////////
 /*
   You might have noticed a slight bug in the previous problem. If you were to pass in 4 gold, the function would return to you 1.3333... However, you can't really go to a store and by 1.333 products. You would just be able to purchase 1 product. Re-write the function you used in the previous problem (give it the same name, just add a 2 to the end of it) that fixes this bug. Invoke the function and store the returned value to a variable called `totalFrogs2`.
