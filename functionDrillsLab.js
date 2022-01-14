e167b6a5648dfc59f196bc12fcce618a42e5198d
Daniel Silva <silvaldaniel70@gmail.com>
2022-01-13 22:32:51 -0600

    all done

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -373,6 +373,18 @@ console.log(emailCheck(email))
 //CODE HERE
 
 function frogCost(gold) {
+  return gold / 3
+}
+ 
+let totalFrogs = frogCost(8)
+console.log(totalFrogs)
+////////////////// PROBLEM 20 ////////////////////
+/*
+  You might have noticed a slight bug in the previous problem. If you were to pass in 4 gold, the function would return to you 1.3333... However, you can't really go to a store and by 1.333 products. You would just be able to purchase 1 product. Re-write the function you used in the previous problem (give it the same name, just add a 2 to the end of it) that fixes this bug. Invoke the function and store the returned value to a variable called `totalFrogs2`.
+*/
+
+//CODE HERE
+function frogCost2(gold) {
   if(gold % 3 === 0) {
     return gold / 3
   }else if ((gold - 1) % 3 === 0) {
@@ -381,23 +393,29 @@ function frogCost(gold) {
     return (gold - 2) / 3
   }
 }
-let totalFrogs = frogCost(9)
-console.log(totalFrogs)
-////////////////// PROBLEM 20 ////////////////////
-/*
-  You might have noticed a slight bug in the previous problem. If you were to pass in 4 gold, the function would return to you 1.3333... However, you can't really go to a store and by 1.333 products. You would just be able to purchase 1 product. Re-write the function you used in the previous problem (give it the same name, just add a 2 to the end of it) that fixes this bug. Invoke the function and store the returned value to a variable called `totalFrogs2`.
-*/
-
-//CODE HERE
+let totalFrogs2 = frogCost2(9)
+console.log(totalFrogs2)
 
 
 ////////////////// PROBLEM 21 ////////////////////
 let sampleArray = [0,1,2,3,4,7,5,6,8,9]
+let sampleArray2 = [0,1,2,3,4,5]
 /*
   Write a function that takes in an array of numbers as an argument. In the body of the function, write logic to determine if the array is in ascending order. The function should return true, if it is sorted in ascending order, false if it is not. Create a variable, `arrayIsAscending` and set it equal to your function invoked. Use the sample array to test this function.
 */
 
 //CODE HERE
+function food(numArr) {
+  for(let i = 0; i < numArr.length -1; i++) {
+    if(numArr[i] > numArr[i +1]) {
+      return false 
+    }
+  } 
+      return true
+}
+
+console.log(food(sampleArray))
+console.log(food(sampleArray2))
 
 
 ////////////////// PROBLEM 22 ////////////////////
@@ -422,13 +440,13 @@ function pond() {
 */
 
 //This array should contain the variable names (as strings) accessible in the global scope.
-let globalScope = []
+let globalScope = ['duck']
 
 //This array should contain the variable names (as strings) accessible in the bathroom function.
-let bathroomScope = []
+let bathroomScope = ['duck', 'rubberDuck']
 
 //This array should contain the variable names (as strings) accessible in the bathtub function.
-let bathtubScope = []
+let bathtubScope = ['duck','sailorDuck','rubberDuck']
 
 //This array should contain the variable names (as strings) accessible in the pond function.
-let pondScope = []
+let pondScope = ['duck','realDuck']
